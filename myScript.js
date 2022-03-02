4615b04fe514d77258326a9873573d7a3fccd0fc
ttse23 <terence.tse23@gmail.com>
2022-03-02 15:40:29 -0800

    Complete logic first pass

--- myScript.js
+++ myScript.js
@@ -1,77 +1,95 @@
-let playerScore = 0;
-let computerScore = 0;
-
-let buttons = document.querySelectorAll('button')
-let results = document.querySelector('#results')
-let winner = document.querySelector('#winner')
-
-
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        let playerSelection = button.id;
-        let computerSelection = computerPlay();
-        console.log(playerSelection);
-        console.log(computerSelection);
-
-        playRound(playerSelection, computerSelection);
-        console.log(playerScore);
-        console.log(computerScore);
-
-        if (playerScore === 5 || computerScore === 5) {
-            nameWinner();
-            newGame();// function asking for new game - clean scoreboard, as well as html elements
-        }        
-    })
-})
+let playerScore = 0
+let computerScore = 0
 
 // computerPlay function    
 function computerPlay() {
-    const options = ['rock', 'paper', 'scissors'];
-    return options[Math.floor(Math.random() * 3)];
+    const options = ['rock', 'paper', 'scissors']
+    return options[Math.floor(Math.random() * 3)]
 }
 
 // playRound function
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        results.innerText = `Both selected ${playerSelection}. It's a tie!`;
+        roundResult.innerText = `Both selected ${playerSelection}. It's a tie!`
     } else if (playerSelection === 'rock') {
         if (computerSelection == 'scissors') {
-            results.innerText = 'Rock smashes scissors! You win!';
-            playerScore++;
+            roundResult.innerText = 'Rock smashes scissors! You win!'
+            playerScore++
         }
         else {
-            results.innerText = 'Paper covers rock. You lose.';
-            computerScore++;
+            roundResult.innerText = 'Rock is covered by paper. You lose.'
+            computerScore++
         }            
     } else if (playerSelection === 'paper') {
         if (computerSelection === 'rock') {
-            results.innerText = 'Paper covers rock! You win!';
-            playerScore++;
+            roundResult.innerText = 'Paper covers rock! You win!'
+            playerScore++
         }
         else {
-            results.innerText = 'Scissors cut paper. You lose.';
-            computerScore++;
+            roundResult.innerText = 'Paper is cut by scissors. You lose.'
+            computerScore++
         }
     } else if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') {
-            results.innerText = 'Scissors cut paper! You win!';
-            playerScore++;
+            roundResult.innerText = 'Scissors cut paper! You win!'
+            playerScore++
         }
         else {
-            results.innerText = 'Rock smashes scissors. You lose.';
-            computerScore++;
+            roundResult.innerText = 'Scissors is smashed rock. You lose.'
+            computerScore++
         }
     }
 }
 
+let buttons = document.querySelectorAll('.selection')
+let roundResult = document.querySelector('#roundResult')
+let finalResult = document.querySelector('#finalResult')
+let playAgainButton = document.querySelector('#playAgain')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (playerScore === 5 || computerScore === 5) {
+            return;
+        }
+        let playerSelection = button.id;
+        let computerSelection = computerPlay();
+        console.log(playerSelection); //
+        console.log(computerSelection); //
+
+        playRound(playerSelection, computerSelection);
+        console.log(playerScore); //
+        console.log(computerScore); //
+        updateScore() 
+
+        if (playerScore === 5 || computerScore === 5) {
+            nameWinner();
+            playAgainButton.classList.toggle('hidden')
+        }        
+    })
+})
+
+playAgainButton.addEventListener('click', newGame)
+
+function updateScore() {
+    document.querySelector('.playerScore').innerText = `${playerScore}`
+    document.querySelector('.computerScore').innerText = `${computerScore}`
+}
+
+
 function nameWinner() {
     if (playerScore > computerScore) {
-        winner.innerText = `Congratulations, you beat the computer! The final score was Player: ${playerScore} - Computer: ${computerScore}! :)`;
+        finalResult.innerText = `Congratulations, you won ${playerScore} - ${computerScore}! :)`;
     }
     else {
-        winner.innerText = `Sorry, you lost to the computer. The final score was Player: ${playerScore} - Computer: ${computerScore}. :(`;
+        finalResult.innerText = `Sorry, you lost ${playerScore} - ${computerScore} :(`;
     }
 }
 
 function newGame() {
+    playerScore = 0
+    computerScore = 0
+    updateScore()
+    playAgainButton.classList.toggle('hidden')
+    roundResult.innerText =''
+    finalResult.innerText = ''
 }
